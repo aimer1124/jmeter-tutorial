85ffb150921345957e14439e09ad16c6729a85e7
aimer1124 <380774061@qq.com>
2019-02-14 15:57:50 +0800

    Delate JMeter Cache; Delete cat fileNamelist

--- runScripts.sh
+++ runScripts.sh
@@ -2,8 +2,6 @@
 
 find ./src/ -name "*.jmx" > fileName.txt
 
-cat fileName.txt
-
 for file in $(<fileName.txt)
 do
     result=$(./apache-jmeter-5.0/bin/jmeter -n -t $file | grep 'Err:' | sed 's/.*(\([0-9]*\).*/\1/g')
