61dff881adc3d988a2f0d7a8466eea50cff6c6ef
aimer1124 <380774061@qq.com>
2019-05-20 11:05:00 +0800

    Fix:https://github.com/aimer1124/jmeter-tutorial/issues/6

--- runScripts.sh
+++ runScripts.sh
@@ -2,14 +2,26 @@
 
 find ./src/ -name "*.jmx" > fileName.txt
 
+scriptsCount=0
+successCount=0
+
 for file in $(<fileName.txt)
 do
+    scriptsCount=$((scriptsCount+1))
     result=$(./apache-jmeter-5.1.1/bin/jmeter -n -t $file | grep 'Err:' | sed 's/.*(\([0-9]*\).*/\1/g')
     if [ $result -gt 0 ]
     then
         echo $file failed
-        exit 1
     else
+        successCount=$((successCount+1))
         echo $file sucess
     fi
 done
+
+if [ "$successCount" -eq "$scriptsCount" ]
+    then
+        echo All $successCount scirpts execute success
+    else
+        echo $successCount success, and $scriptsCount scriptsFile exist.
+        exit 1
+fi
